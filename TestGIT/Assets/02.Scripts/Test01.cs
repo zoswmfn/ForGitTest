edf91b012a51f902feda42982ffee110431dc648
DESKTOP-FML53UE\adam <zoswmfn@gmail.com>
2018-01-31 09:03:41 +0900

    adsfsdfasdf

---
+++ TestGIT/Assets/02.Scripts/Test01.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test01 : MonoBehaviour {
+
+    public int _puAdam;
+    private int _piAdam;
+	
+    // Use this for initialization
+	void Start () {
+        		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void printf() {
+        Debug.Log("Test1");
+    }
+}
